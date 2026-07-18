4a8f1657b8112ddbcafb86478630573697bc3930
thedabbacentral <thedabbacentral@gmail.com>
2026-07-18 14:24:33 +0530

    cors update

--- notion-server/server.js
+++ notion-server/server.js
@@ -6,7 +6,16 @@ import { Client } from "@notionhq/client";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Local React app
+      "https://thedabbacentralapplication.onrender.com", // Your Vercel app
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
